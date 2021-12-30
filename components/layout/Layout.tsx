9b8be66f3f3aed20c09e2a9ea042dd63f19e1662
ali-anas <anasalialmora@gmail.com>
2021-12-30 14:18:52 +0530

    converted layout.js to Layout.tsx

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../../styles/utils.module.css";
+import React from "react";
+
+export const siteTitle = "To-Do App";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Simple to-do using nextjs" />
+
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+
+      {/* header */}
+      <header className={styles.header}>
+        <h2 className={utilStyles.headingLg}>Todo List</h2>
+      </header>
+
+      {/* body */}
+      <main className={styles.main}>{children}</main>
+
+      {/* footer */}
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  );
+}
